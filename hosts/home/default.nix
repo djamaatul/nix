cee2f569fa8f2e5460796e407432cbcab7e44a66
djamaatul <djamaatul.anbiya@gmail.com>
2026-02-23 07:43:04 +0700

    fix: condition system home-manager

--- hosts/home/default.nix
+++ hosts/home/default.nix
@@ -1,11 +1,11 @@
-{ nixgl, ... }:
+{ nixgl, pkgs, ... }:
 {
   programs = {
     fish.enable = true;
   };
 
-  targets.genericLinux.nixGL = { packages = nixgl.packages; };
-  targets.darwin.linkApps.enable = true;
+  targets.genericLinux.nixGL = pkgs.lib.mkIf pkgs.stdenv.isLinux { packages = nixgl.packages; };
+  targets.darwin.linkApps.enable = pkgs.lib.mkIf pkgs.stdenv.isDarwin true;
 
   imports = [
     ../../modules/linux/sway.nix
